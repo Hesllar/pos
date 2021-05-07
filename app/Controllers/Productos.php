a284bd2078d3114aaf5c93cc38c8bc10f78264e6
Bastiaan <bastian.b.d@gmail.com>
2021-05-07 01:00:11 -0400

    cambios en controladores y vistas

--- app/Controllers/Productos.php
+++ app/Controllers/Productos.php
@@ -4,23 +4,30 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\ProductosModel;
+use App\Models\CategoriaModel;
+use App\Models\ConfiguracionModel;
 
 class Productos extends BaseController
 {
     protected $productos;
+    protected $categorias;
 
     public function __construct()
     {
         $this->productos = new ProductosModel();
+        $this->categorias = new CategoriaModel();
+        $this->configuracion = new ConfiguracionModel;
     }
 
     public function index()
     {
         $productos = $this->productos->findAll();
-        $data = ['titulo' => 'Productos', 'datos' => $productos];
+        $categorias = $this->categorias->findAll();
+        $configuracion = $this->configuracion->First();
+        $data = ['titulo' => 'Productos', 'datos' => $productos, 'categorias' => $categorias, 'configuracion' => $configuracion];
 
-        echo view('header');
+        echo view('header', $data);
         echo view('Productos/Productos', $data);
-        echo view('footer');
+        echo view('footer', $data);
     }
 }
---
+++ app/Models/CategoriaModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+use CodeIgniter\Model;
+
+class CategoriaModel extends Model
+{
+
+    protected $table      = 'categoria';
+    protected $primaryKey = 'id_categoria';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = ['nombre_categoria'];
+
+    protected $useTimestamps = false;
+    /*
+    protected $createdField  = 'fecha_registro';
+    protected $updatedField  = 'fecha_entrega';
+    protected $deletedField  = 'deleted_at';
+    */
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+}
+?>
